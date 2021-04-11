af3e47875368b71fbbde918b3454cf2a0148b90a
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-04-11 12:13:21 -0700

    Initial Commit

---
+++ Implement_Crypto_Domain_Resolution/Implement_Crypto_Domain_ResolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Implement_Crypto_Domain_ResolutionApp.swift
+//  Implement_Crypto_Domain_Resolution
+//
+//  Created by hectorsvill on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Implement_Crypto_Domain_ResolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
