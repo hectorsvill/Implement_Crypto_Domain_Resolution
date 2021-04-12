dab1cca1182a48fe402f53bae90e7b4a929000ac
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-04-11 18:31:10 -0700

    basic UI

--- Implement_Crypto_Domain_Resolution/ContentView.swift
+++ Implement_Crypto_Domain_Resolution/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fullText: String = "Domain"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            VStack(alignment: .center) {
+                Text("Address or Domain")
+                    .font(.footnote)
+                    .padding(.top, 10)
+                    .offset(x: -100, y: 10)
+                
+                TextField("Enter Domain", text: $fullText)
+                    .padding()
+                    .foregroundColor(.green)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(8)
+//                    .clipShape(Circle())
+                    
+                Spacer()
+            }
+            .navigationBarTitle("Unstoppable Domains", displayMode: .inline)
+        }
     }
 }
 
