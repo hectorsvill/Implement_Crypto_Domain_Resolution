d68041664ac5ca6877c66786c1cbf0cfbf22aa0f
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-04-12 17:30:27 -0700

    Account

---
+++ Implement_Crypto_Domain_Resolution/Account.swift
@@ -0,0 +1,16 @@
+//
+//  Account.swift
+//  Implement_Crypto_Domain_Resolution
+//
+//  Created by hectorsvill on 4/12/21.
+//
+
+import Foundation
+
+struct Account {
+    let domain: String
+    let address: String
+    
+    let btcAddress = ""
+    let ethAddress = ""
+}
